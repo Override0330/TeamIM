26276231a1248901a5f926372c7a1ca836ebfedd
Override0330 <854098072@qq.com>
2019-08-25 00:10:58 +0800

    fix

--- app/src/main/java/com/override0330/teamim/viewmodel/LoginRegisterViewModel.kt
+++ app/src/main/java/com/override0330/teamim/viewmodel/LoginRegisterViewModel.kt
@@ -1,5 +1,6 @@
 package com.override0330.teamim.viewmodel
 
+import android.os.AsyncTask
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -86,51 +87,53 @@ class LoginRegisterViewModel : BaseViewModel(){
             override fun done(e: AVException?) {
                 if (e==null){
                     //注册成功，自动登录
-                    AVUser.logIn(account,password)
-                    AVUser.logInInBackground(account,password,object :LogInCallback<AVUser>(){
-                        override fun done(user: AVUser?, e: AVException?) {
-                            if (e==null){
-                                if (user!=null){
-                                    with(NowUser.getInstant()){
-                                        nowAVuser = user
-                                        //直接登录IM服务器
-                                        nowClient = AVIMClient.getInstance(user)
-                                        nowClient.open(object : AVIMClientCallback(){
-                                            override fun done(client: AVIMClient?, e: AVIMException?) {
-                                                Log.d("debug","done回调")
-                                                if (e == null) {
-                                                    Log.d("LeanCloud","成功打开IMClient链接")
-                                                    //设置全局监听
-                                                    AVIMMessageManager.setConversationEventHandler(CustomConversationEventHandler())
-                                                    AVIMMessageManager.registerDefaultMessageHandler(CustomMessageHandler())
+                    AsyncTask.execute {
+                        AVUser.logIn(account,password)
+                        AVUser.logInInBackground(account,password,object :LogInCallback<AVUser>(){
+                            override fun done(user: AVUser?, e: AVException?) {
+                                if (e==null){
+                                    if (user!=null){
+                                        with(NowUser.getInstant()){
+                                            nowAVuser = user
+                                            //直接登录IM服务器
+                                            nowClient = AVIMClient.getInstance(user)
+                                            nowClient.open(object : AVIMClientCallback(){
+                                                override fun done(client: AVIMClient?, e: AVIMException?) {
+                                                    Log.d("debug","done回调")
+                                                    if (e == null) {
+                                                        Log.d("LeanCloud","成功打开IMClient链接")
+                                                        //设置全局监听
+                                                        AVIMMessageManager.setConversationEventHandler(CustomConversationEventHandler())
+                                                        AVIMMessageManager.registerDefaultMessageHandler(CustomMessageHandler())
 //                                                    AVIMMessageManager.registerMessageHandler(AVIMImageMessage::class.java,ImageMessageHandler())
-                                                    AVInstallation.getCurrentInstallation().saveInBackground(object :SaveCallback(){
-                                                        override fun done(e: AVException?) {
-                                                            if (e==null){
-                                                                val installationId = AVInstallation.getCurrentInstallation().installationId
-                                                                Log.d("installationId",installationId)
-                                                            }else{
-                                                                e.printStackTrace()
+                                                        AVInstallation.getCurrentInstallation().saveInBackground(object :SaveCallback(){
+                                                            override fun done(e: AVException?) {
+                                                                if (e==null){
+                                                                    val installationId = AVInstallation.getCurrentInstallation().installationId
+                                                                    Log.d("installationId",installationId)
+                                                                }else{
+                                                                    e.printStackTrace()
+                                                                }
                                                             }
-                                                        }
-                                                    })
-                                                    PushService.setDefaultPushCallback(BaseApp.context(),
-                                                        MainActivity::class.java)
-                                                    state.postValue(LoginRegisterState.SUCCESS)
-                                                }else{
-                                                    state.postValue(LoginRegisterState.FAIL)
+                                                        })
+                                                        PushService.setDefaultPushCallback(BaseApp.context(),
+                                                            MainActivity::class.java)
+                                                        state.postValue(LoginRegisterState.SUCCESS)
+                                                    }else{
+                                                        state.postValue(LoginRegisterState.FAIL)
+                                                    }
                                                 }
-                                            }
-                                        })
+                                            })
+                                        }
+                                    }else{
+                                        state.postValue(LoginRegisterState.FAIL)
                                     }
                                 }else{
                                     state.postValue(LoginRegisterState.FAIL)
                                 }
-                            }else{
-                                state.postValue(LoginRegisterState.FAIL)
                             }
-                        }
-                    })
+                        })
+                    }
                 }else{
                     e.printStackTrace()
                     state.postValue(LoginRegisterState.FAIL)
